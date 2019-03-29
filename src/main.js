94476abf35ea317eedd79562f7b8a26a9868b416
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2019-03-29 15:53:26 +0300

    add test data to app

--- src/main.js
+++ src/main.js
@@ -22,6 +22,53 @@ const filters = [
   }
 ];
 
+const OFFERS = [`Add luggage`, `Switch to comfort class`, `Add meal`, `Choose seats`];
+
+// Формирует множество со случайным набором из OFFERS
+const getOffers = () => {
+  const offers = new Set();
+  let amount = Math.floor(Math.random() * (2 + 1));
+  while (amount > 0) {
+    let setSize = offers.size;
+    offers.add(OFFERS[Math.floor(Math.random() * 4)]);
+    if (offers.size > setSize) {
+      amount--;
+    }
+  }
+
+  return offers;
+};
+
+// Тестовые данные для точки путешествия
+const createTripPoint = () => ({
+  type: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10][Math.floor(Math.random() * 10)],
+  name: [
+    `Taxi to Airport`,
+    `Flight to Geneva`,
+    `Drive to Chamonix`,
+    `Check into a hotel`
+  ][Math.floor(Math.random() * 4)],
+  // 43200000 (12 часов) - 1800000 (30 мин)
+  timeDuration: Math.floor(Math.random() * (43200000 + 1 - 1800000)) + 1800000, // 30m - 12h,
+  // от 1 до 1000
+  price: Math.floor(Math.random() * 1000) + 1,
+  offers: getOffers(),
+  img: `http://picsum.photos/300/150?r=${Math.random()}`,
+  descr: [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+    `Fusce tristique felis at fermentum pharetra.`,
+    `Aliquam id orci ut lectus varius viverra.`,
+    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+    `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+    `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+    `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+    `Aliquam erat volutpat.`,
+    `Nunc fermentum tortor ac porta dapibus.`,
+    `In rutrum ac purus sit amet tempus.`
+  ]
+});
+
 
 // Удаляет все события
 const resetPoints = () => {
@@ -29,10 +76,10 @@ const resetPoints = () => {
 };
 
 // Отрисовывает события в количесте count
-const renderTripPoints = (count) => {
+const renderTripPoints = (amount) => {
   const tripPoints = document.createDocumentFragment();
-  for (let i = 0; i < count; i++) {
-    const point = getTripPoint();
+  for (let i = 0; i < amount; i++) {
+    const point = getTripPoint(createTripPoint());
     tripPoints.appendChild(point);
   }
   POINTS_NODE.appendChild(tripPoints);
--- src/trip-point.js
+++ src/trip-point.js
@@ -1,21 +1,42 @@
 // trip-point.js
 
-const getTripPoint = () => {
-  const template = `<i class="trip-icon">🚕</i>
-  <h3 class="trip-point__title">Taxi to Airport</h3>
+const TripTypeEnum = {
+  TAXI: 1,
+  BUS: 2,
+  TRAIN: 3,
+  SHIP: 4,
+  TRANSPORT: 5,
+  DRIVE: 6,
+  FLIGHT: 7,
+  CHECKIN: 8,
+  SIGHTSEEING: 9,
+  RESTAURANT: 10,
+  properties: {
+    1: {name: `Taxi`, icon: `🚕`},
+    2: {name: `Bus`, icon: `🚌`},
+    3: {name: `Train`, icon: `🚂`},
+    4: {name: `Ship`, icon: `️🛳️`},
+    5: {name: `Transport`, icon: `🚊`},
+    6: {name: `Drive`, icon: `🚗`},
+    7: {name: `Flight`, icon: `️✈️`},
+    8: {name: `Check-in`, icon: `🏨`},
+    9: {name: `Sightseeing`, icon: `️🏛️`},
+    10: {name: `Restaurant`, icon: `🍴`},
+  }
+};
+
+
+const getTripPoint = (point) => {
+  const template = `<i class="trip-icon">${TripTypeEnum.properties[point.type].icon}</i>
+  <h3 class="trip-point__title">${point.name}</h3>
   <p class="trip-point__schedule">
     <span class="trip-point__timetable">10:00&nbsp;&mdash; 11:00</span>
     <span class="trip-point__duration">1h 30m</span>
   </p>
-  <p class="trip-point__price">&euro;&nbsp;20</p>
-  <ul class="trip-point__offers">
-    <li>
-      <button class="trip-point__offer">Order UBER +&euro;&nbsp;20</button>
-    </li>
-    <li>
-      <button class="trip-point__offer">Upgrade to business +&euro;&nbsp;20</button>
-    </li>
-  </ul>`;
+  <p class="trip-point__price">&euro;&nbsp;${point.price}</p>
+  <ul class="trip-point__offers">${[...point.offers]
+    .map((offer) => `<li><button class="trip-point__offer">${offer}</button></li>`)
+    .join(``)}</ul>`;
 
   const article = document.createElement(`article`);
   article.classList.add(`trip-point`);
